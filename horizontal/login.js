e807c4f29300c5f7169561ad035de994bf5c93f0
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2024-03-26 16:15:30 -0600

    formating

--- horizontal/login.js
+++ horizontal/login.js
@@ -7,71 +7,72 @@ let nurseFlag = false;
 let current;
 
 function clickReceptionist() {
-    receptionistFlag = !receptionistFlag;
-    doctorFlag = false;
-    nurseFlag = false;
+  receptionistFlag = !receptionistFlag;
+  doctorFlag = false;
+  nurseFlag = false;
 
-    current = 1;
-    update();
+  current = 1;
+  update();
 
-    // Toggle selected border class
-    document.getElementById("receptionistBtn").classList.toggle("selectedBorder");
-    // Remove selected border from other buttons
-    document.getElementById("doctorBtn").classList.remove("selectedBorder");
-    document.getElementById("nurseBtn").classList.remove("selectedBorder");
+  // Toggle selected border class
+  document.getElementById("receptionistBtn").classList.toggle("selectedBorder");
+  // Remove selected border from other buttons
+  document.getElementById("doctorBtn").classList.remove("selectedBorder");
+  document.getElementById("nurseBtn").classList.remove("selectedBorder");
 }
 
 function clickDoctor() {
-    doctorFlag = !doctorFlag;
-    receptionistFlag = false;
-    nurseFlag = false;
+  doctorFlag = !doctorFlag;
+  receptionistFlag = false;
+  nurseFlag = false;
 
-    current = 2;
-    update();
+  current = 2;
+  update();
 
-    // Toggle selected border class
-    document.getElementById("doctorBtn").classList.toggle("selectedBorder");
-    // Remove selected border from other buttons
-    document.getElementById("receptionistBtn").classList.remove("selectedBorder");
-    document.getElementById("nurseBtn").classList.remove("selectedBorder");
+  // Toggle selected border class
+  document.getElementById("doctorBtn").classList.toggle("selectedBorder");
+  // Remove selected border from other buttons
+  document.getElementById("receptionistBtn").classList.remove("selectedBorder");
+  document.getElementById("nurseBtn").classList.remove("selectedBorder");
 }
 
 function clickNurse() {
-    nurseFlag = !nurseFlag;
-    receptionistFlag = false;
-    doctorFlag = false;
+  nurseFlag = !nurseFlag;
+  receptionistFlag = false;
+  doctorFlag = false;
 
-    current = 3;
-    update();
+  current = 3;
+  update();
 
-    // Toggle selected border class
-    document.getElementById("nurseBtn").classList.toggle("selectedBorder");
-    // Remove selected border from other buttons
-    document.getElementById("receptionistBtn").classList.remove("selectedBorder");
-    document.getElementById("doctorBtn").classList.remove("selectedBorder");
+  // Toggle selected border class
+  document.getElementById("nurseBtn").classList.toggle("selectedBorder");
+  // Remove selected border from other buttons
+  document.getElementById("receptionistBtn").classList.remove("selectedBorder");
+  document.getElementById("doctorBtn").classList.remove("selectedBorder");
 }
 
 function update() {
-    let receptionistImg = document.getElementById("receptionistImg");
-    let doctorImg = document.getElementById("doctorImg");
-    let nurseImg = document.getElementById("nurseImg");
+  let receptionistImg = document.getElementById("receptionistImg");
+  let doctorImg = document.getElementById("doctorImg");
+  let nurseImg = document.getElementById("nurseImg");
 
-    if (receptionistFlag) {
-        receptionistImg.src = 'images/receptionist.png';
-        
-    } else {
-        receptionistImg.src = 'images/receptionistGray.png';
-    }   
+  if (receptionistFlag) {
+    receptionistImg.src = "images/receptionist.png";
+  } else {
+    receptionistImg.src = "images/receptionistGray.png";
+  }
 
-    if (doctorFlag) {
-        doctorImg.src = 'images/doctor.png';
-    } else {
-        doctorImg.src = 'images/doctorGray.png'
-    }
+  if (doctorFlag) {
+    doctorImg.src = "images/doctor.png";
+  } else {
+    doctorImg.src = "images/doctorGray.png";
+  }
 
-    if (nurseFlag) {
-        nurseImg.src = 'images/nurse.png';
-    } else {
-        nurseImg.src = 'images/nurseGray.png';
-    }
+  if (nurseFlag) {
+    nurseImg.src = "images/nurse.png";
+  } else {
+    nurseImg.src = "images/nurseGray.png";
+  }
 }
+
+
