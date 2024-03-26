d2b2c26b52d7bf9794b7b2ab9f23372cd2af2630
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2024-03-26 01:05:40 -0600

    login page changed to have user icons

---
+++ horizontal/login.js
@@ -0,0 +1,27 @@
+function hoverReceptionist(element) {
+    element.setAttribute('src', 'receptionist.png');
+}
+  
+function unhoverReceptionist(element) {
+    element.setAttribute('src', 'receptionistGray.png');
+}
+
+function hoverDoctor(element) {
+    element.setAttribute('src', 'doctor.png');
+}
+  
+function unhoverDoctor(element) {
+    element.setAttribute('src', 'doctorGray.png');
+}
+
+function hoverNurse(element) {
+    element.setAttribute('src', 'nurse.png');
+}
+  
+function unhoverNurse(element) {
+    element.setAttribute('src', 'nurseGray.png');
+}
+
+function clickReceptionist(element) {
+    element.setAttribute('src', 'receptionist.png');
+}
