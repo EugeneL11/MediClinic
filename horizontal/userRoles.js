ddb93aac496a7b9da380d2ecb4c4c9bf2f5be40a
EugeneL11 <eugenelee0605@gmail.com>
2024-04-04 13:57:44 -0600

    only receptionist can edit appointments

--- horizontal/userRoles.js
+++ horizontal/userRoles.js
@@ -1,13 +1,27 @@
+let currentUserNum = localStorage.getItem("role");
+
 // Call modifyNavbar() to dynamically modify the navbar
 window.addEventListener('DOMContentLoaded', () => {
+    currentUserNum = localStorage.getItem("role");
     modifyNavbar();
+    bookApptRoles();
 });
 
 function modifyNavbar() {
     const paymentLink = document.getElementById('payID');
-    let currentUserNum = localStorage.getItem("role");
     console.log(currentUserNum); 
     if (currentUserNum == 2 || currentUserNum == 3) {
         paymentLink.style.display = 'none'; // Hide payment link
+    }
+}
+
+function bookApptRoles() {
+    const bookBtn = document.getElementById('book');
+    const editAppt = document.getElementById('editAppointment');
+    const cancelAppt = document.getElementById('cancelAppointment');
+    if (currentUserNum == 2 || currentUserNum == 3) {
+        bookBtn.style.visibility = 'hidden';
+        editAppt.style.visibility = 'hidden';
+        cancelAppt.style.visibility = 'hidden';
     }
 }
